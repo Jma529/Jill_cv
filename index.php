7cb8f8f6f9001c6b58b0a8befbfd89368b25438b
Jill Augustus <jma529@gmail.com>
2019-05-03 15:16:36 +0800

    Change fonts

--- index.php
+++ index.php
@@ -35,11 +35,9 @@
     <div class="inspiration">
       <div class="container-inspo">
         <div class="my-story col-6">
-          <h1>Hello!</h1>
           <h2>My Story</h2>
           <!-- <p>I have recently made a career change to website development from fitness. While they seem in completely different realms, they actually complement each other to complete the perfect package. With coding our minds get the workout, with fitness our bodies do...making the two a great pair.</p> -->
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad vero minima labore alias distinctio
-            aspernatur, magni at dolores natus.</p>
+          <p>I recently have made a career change to website development from the fitness world. I am so excited to help people make beautiful and function websites. With coding our minds get the workout, with fitness our bodies do...making the two very complementary.</p>
         </div>
         <div class="inspo-content col-6 js--fadeInRight">
           <h1>Balance is not something you find,<br> it's something you create</h1>
@@ -51,9 +49,9 @@
 
     <!--Timeline inspired by A PEN BY Sava Lazic and scroll reveal -->
     <section class="timeline">
-        <h1>Education & Experience</h1>
+      <h1>Education & Experience</h1>
       <div class="container-timeline">
-      
+
         <div class="timeline-item">
 
 
@@ -63,196 +61,126 @@
             <p>LA Fit Studio</p>
             <p></p> -->
         </div>
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-graduation-cap left"></i>
           </div>
-          <i class="fas fa-graduation-cap left"></i>
-        </div>
 
-        <div class="timeline-content js--fadeInRight">
-          <!-- <div class="timeline-img-header perth"> -->
-          <h2>Full Stack Web Development Certificate</h2>
-          <h3>Codemaster Institute</h3>
-          <h3>March-June 2019</h3>
-          <p>A 12-week full-time intensive course Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            rerum numquam,
-            iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
-        </div>
-      </div>
-      <div class="timeline-item">
-
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+          <div class="timeline-content js--fadeInRight">
+            <!-- <div class="timeline-img-header perth"> -->
+            <h2>Full Stack Web Development Certificate</h2>
+            <h3>Codemaster Institute</h3>
+            <h3>March-June 2019</h3>
+            <p>A 12-week full-time intensive course Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+              rerum numquam,
+              iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
           </div>
-          <i class="fas fa-briefcase right"></i>
         </div>
-        <div class="timeline-content js--fadeInLeft">
-          <h2>Studio Manager & Teacher Trainer</h2>
-          <h3>LA Fit Studio</h3>
-          <h3>August 2015-October 2018</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum numquam,
-            iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
-        </div>
-      </div>
+        <div class="timeline-item">
 
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-briefcase right"></i>
           </div>
-          <i class="fas fa-graduation-cap left"></i>
-        </div>
-        <div class="timeline-content timeline-card js--fadeInRight">
-          <!-- <div class="timeline-img-header sf"> -->
-          <h2>Build Websites from Scratch credential</h2>
-          <h3>Codecademy</h3>
-          <h3>July 2018</h3>
-          <p>A 10-week online curriculum of web development fundamentals using HTML, CSS, JavaScript, and Github.</p>
-        </div>
-      </div>
-
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+          <div class="timeline-content js--fadeInLeft">
+            <h2>Studio Manager & Teacher Trainer</h2>
+            <h3>LA Fit Studio</h3>
+            <h3>August 2015-October 2018</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum numquam,
+              iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
           </div>
-          <i class="fas fa-hands-helping right"></i>
         </div>
-        <div class="timeline-content timeline-card js--fadeInLeft">
-          <!-- <div class="timeline-img-header sanclemente"> -->
-          <h2>City Lead(Volunteer)</h2>
-          <h3>Sofar Sounds</h3>
-          <h3>Dec 2015-March 2016</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
-            perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
-        </div>
-      </div>
 
-
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-graduation-cap left"></i>
           </div>
-          <i class="fas fa-briefcase left"></i>
-          <span class="line"></span>
-        </div>
-        <div class="timeline-content js--fadeInRight">
-          <h2>Customer Success Manager</h2>
-          <h3>fitmob inc</h3>
-          <h3>August 2015-October 2018</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum numquam,
-            iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
-        </div>
-      </div>
-
-
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+          <div class="timeline-content timeline-card js--fadeInRight">
+            <!-- <div class="timeline-img-header sf"> -->
+            <h2>Build Websites from Scratch credential</h2>
+            <h3>Codecademy</h3>
+            <h3>July 2018</h3>
+            <p>A 10-week online curriculum of web development fundamentals using HTML, CSS, JavaScript, and Github.</p>
           </div>
-          <i class="fas fa-briefcase right"></i>
         </div>
-        <div class="timeline-content timeline-card js--fadeInLeft">
-          <!-- <div class="timeline-img-header sanclemente"> -->
-          <h2>Personal Trainer & Group Fitness Instructor</h2>
-          <h3>Gratify Fitness</h3>
-          <h3>August 2009-2015</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
-            perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
-        </div>
-      </div>
 
-
-      <div class="timeline-item">
-        <div class="timeline_img_icon">
-          <div class="timeline-img">
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-hands-helping right"></i>
           </div>
-          <i class="fas fa-graduation-cap left"></i>
-        </div>
-        <div class="timeline-content js--fadeInRight">
-          <h2>B.A. in Environmental Design</h2>
-          <h3>University of Colorado at Boulder</h3>
-          <h3>2005-2009</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
-            perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
-        </div>
-      </div>
-      </div>
-
-      <!-- <div class="timeline-item boulder">
-
-        <div class="timeline-img"></div>
-
-        <div class="timeline-content timeline-card js--fadeInRight">
-          <div class="timeline-img-header boulder">
-            <h2>Move to Boulder, CO</h2>
+          <div class="timeline-content timeline-card js--fadeInLeft">
+            <!-- <div class="timeline-img-header sanclemente"> -->
+            <h2>City Lead(Volunteer)</h2>
+            <h3>Sofar Sounds</h3>
+            <h3>Dec 2015-March 2016</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
+              perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
           </div>
-          <div class="date">September 2005</div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate
-            saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias,
-            ullam.</p>
+        </div>
 
+
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-briefcase left"></i>
+            <span class="line"></span>
+          </div>
+          <div class="timeline-content js--fadeInRight">
+            <h2>Customer Success Manager</h2>
+            <h3>fitmob inc</h3>
+            <h3>August 2015-October 2018</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur rerum numquam,
+              iusto nesciunt porro consectetur eaque molestias nisi quod laboriosam.</p>
+          </div>
+        </div>
+
+
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-briefcase right"></i>
+          </div>
+          <div class="timeline-content timeline-card js--fadeInLeft">
+            <!-- <div class="timeline-img-header sanclemente"> -->
+            <h2>Personal Trainer & Group Fitness Instructor</h2>
+            <h3>Gratify Fitness</h3>
+            <h3>August 2009-2015</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
+              perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
+          </div>
+        </div>
+
+
+        <div class="timeline-item">
+          <div class="timeline_img_icon">
+            <div class="timeline-img">
+            </div>
+            <i class="fas fa-graduation-cap left"></i>
+          </div>
+          <div class="timeline-content js--fadeInRight">
+            <h2>B.A. in Environmental Design</h2>
+            <h3>University of Colorado at Boulder</h3>
+            <h3>2005-2009</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo consectetur,
+              perferendis neque voluptatibus corporis eveniet numquam expedita ducimus asperiores!</p>
+          </div>
         </div>
       </div>
-
-      <div class="timeline-item">
-
-        <div class="timeline-img"></div>
-
-        <div class="timeline-content js--fadeInLeft">
-          <div class="date">1 SEP 2016</div>
-          <h2>Title</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate
-            saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias,
-            ullam.</p>
-
-        </div>
-      </div> -->
-
-
-
 
     </section>
-    <!-- <div class="introduction grid">
-      <div class="profile col-4">
-        <img class="profile-pic" src="CSS/images/Jillresume.jpeg">
-        <ul class="contact-details">
-          <li>
-            <i class="fas fa-globe-asia"></i><span>Perth, Western Australia</span>
+   
+      
 
-          </li>
-          <li>
-            <a href="mailto:Jma529@gmail.com"><i class="far fa-envelope"></i></a>
-            <span>Jma529@gmail.com</span>
-
-          </li>
-          <li>
-              <i class="fas fa-phone"></i>
-            <span>0404 841 480</span>
-
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/jill-augustus-a930bb10/"><i class="fab fa-linkedin"></i></a>
-            <span>Jill Augustus</span>
-
-          </li>
-        </ul>
-       
-      </div>
-      <div class="description col-8">
-        <h1>Hello!</h1>
-        <h2>My Story</h2>
-        
-        <!-- <p>I have recently made a career change to website development from fitness. While they seem in completely different realms, they actually complement each other to complete the perfect package. With coding our minds get the workout, with fitness our bodies do...making the two a great pair.</p> -->
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil ad vero minima labore alias distinctio
-      aspernatur, magni at dolores natus.</p>
   </div>
-  <!-- <a href="education">
-                <span class="arrow">
-
-                </span>
-            </a> -->
-
-  </div> -->
 
   <!-- <div class="experience col-12">
             <h1>Experience</h1>
@@ -319,10 +247,10 @@
     <div class="profile col-6">
       <img class="profile-pic" src="CSS/images/Jillresume.jpeg">
     </div>
-      <div class="Get in touch col-6">
-          <h1>Get in Touch</h1>
-    
-        <ul class="contact-details">
+    <div class="get-in-touch col-6">
+      <h1>Get in Touch</h1>
+
+      <ul class="contact-details">
         <li>
           <i class="fas fa-globe-asia"></i><span>Perth, Western Australia</span>
 
@@ -343,13 +271,15 @@
 
         </li>
       </ul>
+      <br>
+      <p><em>Eligible to work in Australia and U.S.</em></p>
 
     </div>
 
-   
+
   </div>
 
-
+</div>
 
 
 
